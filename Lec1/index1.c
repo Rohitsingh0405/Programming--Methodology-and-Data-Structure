903f1cc3dbdcd2333f874a8be87aa58747a90e40
Rohit Singh <rs21Rohit@gmail.com>
2024-12-19 08:06:01 +0530

    How to think of a logic

--- Lec1/index1.c
+++ Lec1/index1.c
@@ -2,7 +2,7 @@
 int main(){
 printf("Our \n first Program \n");
 printf("Our \t first Program \n");
-printf('This is nptel lecture on Programming methodology and Data Structure')
+printf('This is nptel lecture on Programming methodology and Data Structure');
 
 return 0;
 }
